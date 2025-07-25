2a6a9566f86c2ce69beacdb05403ab941ebff766
BrunoYamamoto2204 <brunoeiky2006@gmail.com>
2025-07-25 12:03:37 -0300

    Reestruturação completa

---
+++ index.js
@@ -0,0 +1,39 @@
+// Funcionalidades do carrossel
+document.addEventListener("DOMContentLoaded", function() {
+    const swiper = new Swiper('.mySwiper', {
+        slidesPerView: 3,
+        loop: true,
+        spaceBetween: 50,
+        autoplay: {
+            delay: 3000,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        }
+    });
+})
+    
+// Gerenciamento do header
+document.addEventListener("DOMContentLoaded", function () {
+    const header = document.querySelector("header");
+    const sentinela = document.querySelector("#sentinela");
+
+    const observer = new IntersectionObserver(
+        entries => {
+            const entry = entries[0];
+
+            if (entry.isIntersecting) {
+                header.classList.add("header-static");
+            } else {
+                header.classList.remove("header-static");
+            }
+        },
+        {
+            root: null,
+            threshold: 0,
+        }
+    );
+    observer.observe(sentinela);
+    
+});
